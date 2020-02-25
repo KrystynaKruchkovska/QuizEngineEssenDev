6be259a50b083612a1c2192957eec95fd4871b13
Krystyna Kruchkovska <kruchok883@gmail.com>
2020-02-25 15:30:53 +0100

    ResultPresenter tests added

--- QuizEngine/Result.swift
+++ QuizEngine/Result.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 public struct Result<Question:Hashable, Answer> {
-    public let answers:[Question:Answer]?
+    public let answers:[Question:Answer]
     public let score: Int?
     
     public init(answers:[Question: Answer], score: Int) {
